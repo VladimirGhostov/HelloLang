fe5498f2d90aac2d66fcf0b14c92e4cae0c40caf
Ghostov <vladimirghostov@vk.com>
2022-11-04 10:34:24 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿//using System.Threading;
+using System;
+
+void SayHelloRu()
+{
+    Console.WriteLine("Привет!");
+}
+
+void SayHelloEn()
+{
+    Console.WriteLine("Hello!");
+}
+
+void SayHelloFr()
+{
+    Console.WriteLine("Salut!");
+}
+
+void SelLang() => Console.WriteLine("1.Russian \n2.English \n3.French");
+
+void ExitProgram()
+{
+    Console.Clear();
+    Console.WriteLine("Для выхода из программы нажмите любую клавишу...");
+    Console.ReadKey();
+}
+
+Console.WriteLine("Введите язык для приветствия");
+SelLang();
+Console.WriteLine();
+string? language = Console.ReadLine();
+switch (language)
+{
+    case "2":
+        SayHelloEn();
+        break;
+    case "1":
+        SayHelloRu();
+        break;
+    case "3":
+        SayHelloFr();
+        break;
+}
+
+Thread.Sleep(1500);
+ExitProgram();
